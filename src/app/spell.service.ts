7d56fe6ca142600b620bda5dd26bcdf0a734a6f0
Jan Drenkhahn <jan-dennisd@outlook.at>
2021-03-02 21:49:15 +0100

    include error handler

--- src/app/spell.service.ts
+++ src/app/spell.service.ts
@@ -18,8 +18,29 @@ export class SpellService {
   getSpells(): Observable<Spell[]> {
     return this.http.get<Spell[]>(this.spellsUrl)
       .pipe(
+        tap(_ => this.log('fetched heroes')),
         catchError(this.handleError<Spell[]>('getSpells', []))
       )
+  }
+
+  /**
+ * Handle Http operation that failed.
+ * Let the app continue.
+ * @param operation - name of the operation that failed
+ * @param result - optional value to return as the observable result
+ */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 
   getSpell(id: number): Observable<Spell> {
